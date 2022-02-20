f051a4d926a6b29ff6ec1e35ef08af414dc0f462
JeCkaV <jeckavucko@gmail.com>
2022-02-20 19:11:59 +0100

    Update scripts.js
    Started app code from beginning, created array of Pokémon's.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-alert('Hello world');
+let pokemonList = [{
+ name: 'Bulbasaur',
+ height: 7,
+ type: ['grass', 'poison']
+},
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+{
+    name: 'Butterfree',
+    height: 3,
+    type: ['bug']
+},
+
+ {
+     name: 'Nidoran',
+     height: 0.4,
+     type: ['monster', 'field']
+    },
+
+
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: ['fleid', 'fairy']
+    }
+
+    {
+        name: 'Ditto',
+        height: 0.3,
+        type: ['normal']
+    }]
